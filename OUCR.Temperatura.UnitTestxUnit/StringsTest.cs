3b93d17ee805c35b47f16c5de7ac4f213ab4d2f7
oswaldo chilin <oswaldochilin49@gmail.com>
2024-10-08 14:22:27 -0600

    finalizacion de los ejercicios

---
+++ OUCR.Temperatura.UnitTestxUnit/StringsTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OUCR.Strings;
+namespace OUCR.Temperatura.UnitTestxUnit
+{
+    public class StringsTest
+    {
+        [Fact]
+        public void ReverseStrinstestxUnit()
+        {
+            StringManipulator stringManipulator = new StringManipulator();
+
+            Assert.Equal("qwe", stringManipulator.ReverseString("ewq"));
+        }
+
+        [Fact]
+        public void RemoveStrinsxUnit()
+        {
+            StringManipulator strManipulator = new StringManipulator();
+
+            Assert.Equal("casade", strManipulator.RemoveString("casa de"));
+        }
+    }
+}
