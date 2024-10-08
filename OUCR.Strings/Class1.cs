562359d0d0131daf6acbc60a5d229bdffbf9740f
oswaldo chilin <oswaldochilin49@gmail.com>
2024-10-08 14:12:52 -0600

    se creo la biblioteca de calses del ejercicio de strings

---
+++ OUCR.Strings/Class1.cs
@@ -0,0 +1,20 @@
+﻿namespace OUCR.Strings
+{
+    public class StringManipulator
+    {
+        public string ReverseString(string input)
+        {
+            string original = input;
+            char[] charArray = original.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+
+        public string RemoveString(string input)
+        {
+            string original = input;
+            string sinEspacios = original.Replace(" ", "");
+            return sinEspacios;
+        }
+    }
+}
